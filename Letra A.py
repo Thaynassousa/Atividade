a2ecae15d620bdf8ae4638c4f0bab8c56924d9c1
Thaynassousa <thaynasousasimoes@gmail.com>
2026-03-29 22:58:01 -0300

    Update Letra A.py

---
+++ Letra A.py
@@ -0,0 +1,6 @@
+##Elabore um algoritmo que receba dois números, calcule a multiplicação entre eles e apresente o resultado.
+print("vamos fazer uma multiplicação com dois números")
+num1=float(input("digite o primeiro número"))
+num2=float(input("digite o segundo número"))
+resultado=num1*num2
+print(f"o resultado da sua multiplicação é:{resultado}")
